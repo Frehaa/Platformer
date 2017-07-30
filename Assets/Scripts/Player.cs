58fc1eb5cbff29e0ad712da8aa60201120875cd1
Frederik Haagensen <frehaa@hotmail.dk>
2017-07-31 00:00:26 +0200

    Jumping feels somewhat okey here

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,56 +3,363 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D))]
 public class Player : MonoBehaviour {
     [SerializeField]
     private float speed = 5f;
     [SerializeField]
     private MoveSet moveSet;
+    [SerializeField]
+    private float gravity = -10;
+    [SerializeField]
+    private float jumpVelocity = 10;
+    [SerializeField]
+    private float minJumpTime = 1f;
+    [SerializeField]
+    private float jumpDeceleration;
+    [SerializeField]
+    private float maxJumpTime;
+    [SerializeField]
+    private float maxHoveringTime;
 
-    private Rigidbody2D body;
-    private float direction = 0f;
+    [SerializeField]
+    private int horizontalRays;
+    [SerializeField]
+    private int verticalRays;
+    [SerializeField]
+    private float waitTime;
+
+
+    private float horizontalRaySpacing;
+    private float verticalRaySpacing;
+
+    private double jumpTime = 0;
+    private bool isJumping = false;
+
+    private Vector3 velocity = Vector3.zero;
+    private new BoxCollider2D collider;
 
     private bool isGrounded = false;
+    private bool wallHugging = false;  
+    private int mask;
+    private bool isHovering;
+    private float hoveringTime;
+    private float skinWidth = 0.02f;
 
-	// Use this for initialization
-	void Start () {
-        body = GetComponent<Rigidbody2D>();
+    void Start () {
+        collider = GetComponent<BoxCollider2D>();
+        mask = LayerMask.GetMask("World");
+
+        CalculateRaySpacing();
 	}
+
+    private void CalculateRaySpacing()
+    {
+        Bounds bounds = GetBounds();
+        
+        float width = bounds.size.x;
+        float height = bounds.size.y;
+
+        horizontalRaySpacing = height / (horizontalRays - 1);
+        verticalRaySpacing = width / (verticalRays - 1);
+
+    }
 
     private void FixedUpdate()
     {
-        SetVelocityX(direction * speed);
     }
 
-    // Update is called once per frame
-    void Update () {
-        direction = Input.GetAxisRaw("Horizontal");
+    void Update ()
+    {
+        if (Time.fixedTime < waitTime)
+        {   
+            return;
+        }
+            
 
-        if (body.velocity.y == 0)
-            isGrounded = true;
+        //MovementIteration1();
+        MovementIteration2();
+        //MovementIteration3();
+    }
 
-        bool jump = Input.GetAxisRaw("Jump") == 1;
+    private void MovementIteration3()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Jump Start");
+            jumpTime = 0;
+            isJumping = true;
+        }
+
+        if (isJumping)
+        {
+            Debug.Log(jumpTime);
+        }
+
+        if (jumpTime >= 1.0)
+        {
+            isJumping = false;
+        }
+        
+    }
+
+    private void MovementIteration2()
+    {
+        Bounds bounds = GetBounds();
+        Vector2 moveVector;
+        Vector2 origin;
+        RaycastHit2D hit;
+
+        HandleJump();
+        HandleRun();
+
+        moveVector = velocity * Time.deltaTime;
+
+        // Limit vertically
+        if (moveVector.y != 0)
+        {
+            isGrounded = false;
+            int directionY = Math.Sign(moveVector.y);
+
+            Debug.Log(directionY);
+
+            if (IsUp(directionY))
+            {
+                origin = new Vector2(bounds.min.x, bounds.max.y);
+            }
+            else
+            {
+                origin = new Vector2(bounds.min.x, bounds.min.y);
+            }
+
+            for (int i = 0; i < verticalRays; ++i)
+            {
+                Vector2 rayOrigin = origin;
+                rayOrigin.x += verticalRaySpacing * i;
+
+                hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, directionY * moveVector.y + skinWidth, mask);
+                Debug.DrawRay(rayOrigin, Vector2.up * directionY * (moveVector.y + skinWidth));
+
+                if (hit)
+                {
+                    float distance = hit.distance - skinWidth;
+                    moveVector.y = directionY * distance;
+                    velocity.y = 0;
+                    if (directionY == -1)
+                        isGrounded = true;
+                }
+            }
+        }
+
+        // Limit horizontally
+        if (moveVector.x != 0)
+        {
+            int directionX = Math.Sign(moveVector.x);
+
+            if (IsRight(directionX))
+            {
+                origin = new Vector2(bounds.max.x, bounds.min.y);
+            }
+            else
+            {
+                origin = new Vector2(bounds.min.x, bounds.min.y);
+            }
+            for (int i = 0; i < horizontalRays; i++)
+            {
+                Vector2 rayOrigin = origin;
+                rayOrigin.y += horizontalRaySpacing * i;
+
+                hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, directionX * moveVector.x + skinWidth, mask);
+                Debug.DrawRay(rayOrigin, Vector2.right * ((directionX * moveVector.x) + skinWidth));
+
+                if (hit)
+                {
+                    float distance = hit.distance - skinWidth;
+                    moveVector.x = directionX * distance;
+                }
+            }
+
+        }
+
+        transform.Translate(moveVector);
+    }
+
+    private Bounds GetBounds()
+    {
+        Bounds bounds = collider.bounds;
+        bounds.Expand(-2 * skinWidth);
+        return bounds;
+    }
+
+    private void HandleRun()
+    {
+        int horizontal = Math.Sign(Input.GetAxisRaw("Horizontal"));
+        velocity.x = speed * horizontal;
+    }
+
+    private void HandleJump()
+    {
+        if (isJumping)
+        {
+            jumpTime += Time.deltaTime;
+        }
+        if (isHovering)
+            hoveringTime += Time.deltaTime;
+
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            isGrounded = false;
+            isJumping = true;
+            jumpTime = 0;
+            hoveringTime = 0;
+            SetVelocityY(jumpVelocity);
+            Debug.Log("Jump start");
+        }
+        
+        if (isJumping && (Input.GetKeyUp(KeyCode.Space) || jumpTime >= maxJumpTime || velocity.y < 0))
+        {
+            isJumping = false;
+            isHovering = true;
+            Debug.Log("Jump end");
+            Debug.Log(transform.position);
+        }
+        if (isHovering && hoveringTime >= maxHoveringTime)
+        {
+            isHovering = false;
+        }
+
+        if (isJumping)
+            DecelerateJump();
+        else if (isHovering)
+            SetVelocityY(0);
+        else
+            AddGravity();
+    }
+
+    private void MovementIteration1()
+    {
+        Bounds bounds = collider.bounds;
+        Vector2 moveVector;
+        Vector2 origin;
+        RaycastHit2D hit;
+
+        // Set Velocity according to 
+        AddGravity();
+
+        bool jump = Input.GetKeyDown(KeyCode.Space);
 
         if (jump && isGrounded)
         {
-            SetVelocityY(5f);
+            SetVelocityY(jumpVelocity);
+            isGrounded = false;
+        }
+        else if (jump && wallHugging)
+        {
+            SetVelocityY(jumpVelocity);
+            SetVelocityX(3f);
+            isGrounded = false;
+        }
+
+        int directionX = Math.Sign(Input.GetAxisRaw("Horizontal"));
+        SetVelocityX(directionX * speed);
+
+        wallHugging = false;
+
+        // Limit movement 
+
+        moveVector = velocity * Time.deltaTime;
+
+        // Limit vertically 
+        int directionY = Math.Sign(moveVector.y);
+
+        if (IsUp(directionY))
+        {
+            origin = new Vector2(bounds.center.x, bounds.max.y);
+        }
+        else
+        {
+            origin = new Vector2(bounds.center.x, bounds.min.y);
         }
 
 
+        hit = Physics2D.Raycast(origin, Vector2.up, moveVector.y, mask);
+        Debug.DrawRay(origin, Vector2.up * moveVector.y);
 
+        if (hit)
+        {
+            moveVector.y = directionY * hit.distance;
+            velocity.y = 0;
+            if (directionY == -1)
+                isGrounded = true;
+        }
+
+        // Limit horizontally
+
+        if (directionX != 0)
+        {
+            if (IsRight(directionX))
+            {
+                origin = new Vector2(bounds.max.x, bounds.center.y);
+            }
+            else
+            {
+                origin = new Vector2(bounds.min.x, bounds.center.y);
+            }
+
+
+            hit = Physics2D.Raycast(origin, Vector2.right * directionX, moveVector.x, mask);
+            Debug.DrawRay(origin, Vector2.right * directionX * moveVector.x);
+
+            if (hit)
+            {
+                transform.Translate(Vector2.right * directionX * hit.distance);
+                wallHugging = true;
+                moveVector.x = 0;
+            }
+        }
+
+
+        // Move Character
+        transform.Translate(moveVector);
     }
 
     private void SetVelocityX(float velocityX)
     {
-        Vector3 newVelocity = body.velocity;
-        newVelocity.x = velocityX;
-        body.velocity = newVelocity;
+        velocity.x = velocityX;
     }
 
     private void SetVelocityY(float velocityY)
     {
-        Vector3 newVelocity = body.velocity;
-        newVelocity.y = velocityY;
-        body.velocity = newVelocity;
+        velocity.y = velocityY;
+    }
+
+    private void AddGravity()
+    {
+        velocity.y += gravity * Time.deltaTime;
+    }
+
+    private void DecelerateJump()
+    {
+        velocity.y -= jumpDeceleration * Time.deltaTime;
+    }
+
+    private bool IsUp(int sign)
+    {
+        if (sign == 1)
+            return true;
+        else if (sign == -1)
+            return false;
+        else
+            throw new ArgumentException();
+    }
+
+    private bool IsRight(int sign)
+    {
+        if (sign == 1)
+            return true;
+        else if (sign == -1)
+            return false;
+        else
+            throw new ArgumentException();
     }
 }
